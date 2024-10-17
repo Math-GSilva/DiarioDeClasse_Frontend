c405013823c30b0faf9f126350e1675accbe9f33
Eduardo da Maia Haak <eduardodamaiahaak@gmail.com>
2024-10-17 02:30:55 -0300

    Initial Angular Project

---
+++ DiarioDeClasse/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+    {'path': '', 
+    loadChildren: () => import('./modules/login-page/login-page.module').then(m => m.LoginPageModule)},
+];
