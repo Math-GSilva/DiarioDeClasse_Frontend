4340f2ea741d18cd7401c9d23361042c461258cd
Eduardo da Maia Haak <eduardodamaiahaak@gmail.com>
2024-11-09 01:36:09 -0300

    Ajustes no componente page-select

--- DiarioDeClasse/src/app/components/page-select/page-select.component.ts
+++ DiarioDeClasse/src/app/components/page-select/page-select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-page-select',
@@ -11,4 +11,28 @@ export class PageSelectComponent {
   @Input() first_page: number = 1;
   @Input() current_page: number = 2;
   @Input() last_page: number = 12;
+
+  @Output() nextPage = new EventEmitter<void>();
+  @Output() previousPage = new EventEmitter<void>();
+  @Output() selectPage = new EventEmitter<number>();
+
+  goToNextPage() {
+    this.nextPage.emit();
+  }
+
+  goToPreviousPage() {
+    this.previousPage.emit();
+  }
+
+  goToSelectPage(value: number){
+    this.selectPage.emit(value);
+  }
+
+  onEnterPress(value: string) {
+    const pageNumber = Number(value);
+
+    if( pageNumber > this.first_page && pageNumber < this.last_page ){
+      this.goToSelectPage(pageNumber);
+    }
+  }
 }
